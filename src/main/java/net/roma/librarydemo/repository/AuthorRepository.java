7ee8d9301d87786cdd973566ee4d46c6661220d8
vermutr <vertinsky.vermut@gmail.com>
2020-12-03 12:31:56 +0100

    Author, Book, User repository has been added

--- src/main/java/net/roma/librarydemo/repository/AuthorRepository.java
+++ src/main/java/net/roma/librarydemo/repository/AuthorRepository.java
@@ -1,10 +1,7 @@
 package net.roma.librarydemo.repository;
 
 import net.roma.librarydemo.model.Author;
-import net.roma.librarydemo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
